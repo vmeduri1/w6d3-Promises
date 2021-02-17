9facf5963d9146f61b5923900870a85ab5128bfd
Qingwei <qchen6060@gmail.com>
2021-02-17 16:18:24 -0500

    phase 4 started

--- promises.js
+++ promises.js
@@ -5,8 +5,25 @@ async function num2() {
     return 2;
 }
 
-console.log('num1', num1());
-console.log('num2', num2());
+// console.log('num1', num1());
+// console.log('num2', num2());
 
-num2().then(result => console.log(result));
+//num2().then(result => console.log(result));
 
+async function waiting() {
+    const value = await num2();
+   console.log('waiting', value);
+}
+waiting();
+
+async function waitForMyPromise(){
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('done!!!');
+        }, 1000)
+    });
+
+    const result = await promise;
+    console.log('my promise is', result);
+}
+ waitForMyPromise();
